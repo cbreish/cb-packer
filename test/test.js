f8b9fdcc52730e4ce05e45bd1369eca9744aa743
cbreish <cbreish@gmail.com>
2016-07-20 22:46:07 -0400

    Added some tests for reading json config files.

--- test/test.js
+++ test/test.js
@@ -2,8 +2,28 @@ const assert = require('chai').assert;
 const PackerFile = require('./../PackerFile');
 
 describe ('PackerFile', function () {
+	var file;
 	it('should be able to be instantiated', function() {
-		var file = new PackerFile();
-		assert.notEqual(file, null);
+		file = new PackerFile();
+		assert.instanceOf(file, PackerFile);
+	});
+	describe('Read', function() {
+		it('should read a file', function(done) {
+			file.Read('./test/testfile1.json', function() {
+				done();
+			});
+		});
+		it('should parse variables', function() {
+			assert.equal(file.variables.variable1, 'value1');
+		});
+		it('should parse builders', function() {
+			assert.equal(file.builders[0].type, 'digitalocean');
+		});
+		it('should parse provisioners', function() {
+			assert.equal(file.provisioners[0].type, 'shell');
+		});
+		it('should parse post-processors', function() {
+			assert.equal(file.postprocessors[0], 'compress');
+		});
 	});
 });
